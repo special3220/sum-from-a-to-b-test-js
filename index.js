9a89a2aaaf1b24e91d1cd211ad1114098b46b537
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-04-30 21:37:41 -0400

    function implemented

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(fromN === toN) {
+    return fromN;
+  }
+  return fromN + sum(fromN + 1, toN )
 }
+
+console.log(sum(1, 5))
 
 module.exports = sum;
